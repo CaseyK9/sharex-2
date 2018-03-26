60b7f3fd674e948baadf2afa6c0becb93c4d89dd
bignunxd <bignunxd@gmail.com>
2018-03-27 03:27:26 +0700

    ...

--- travel/views.py
+++ travel/views.py
@@ -79,7 +79,7 @@ class GetTravelViewSet(mixins.CreateModelMixin,
 				if len(sort_list)==0:
 					LIST['status'].append('no request')
 				else:
-					sort_list = sorted(sort_list, key=lambda x: x[2])
+					sort_list = sorted(sort_list, key=lambda x: x[1])
 					for i in range(0,k):
 						if i<5:
 							LIST['details'].append(tmp_list[i])
