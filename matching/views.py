d023a3d748d45d19afa667a4c6a937dd89effd76
bignunxd <bignunxd@gmail.com>
2018-04-03 04:52:30 +0700

    multiple v2

--- matching/views.py
+++ matching/views.py
@@ -12,7 +12,7 @@ from travel.models import Travel
 from request.models import Request
 from account.models import Account
 import json
-import base64
+import requests
 
 class GetMatching_Detail(mixins.CreateModelMixin,
 					    viewsets.GenericViewSet):
