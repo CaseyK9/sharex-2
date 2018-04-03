b3bf6ac836c928ff3ee7979a06814054e83a8ca9
bignunxd <bignunxd@gmail.com>
2018-04-03 15:25:46 +0700

    multiple v2

--- matching/views.py
+++ matching/views.py
@@ -84,5 +84,5 @@ class Get_Multiple_Matching(mixins.CreateModelMixin,
 			payload = {'location':location}
 			headers = {'Authorization':"Basic c2hhcmV4c2VydmVyOnNoYXJleGFkbWlu"}
 			r = requests.post(url, data=json.dumps(payload), headers=headers)
-			return Response(json.dumps(payload))
+			return Response(r.status_code)
 		else: return Response("400")
