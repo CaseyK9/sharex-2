75d8829b48be57a03d44d9eb1aefd54bd7b72aa8
bignunxd <bignunxd@gmail.com>
2018-04-09 04:28:03 +0700

    update station

--- matching/views.py
+++ matching/views.py
@@ -140,7 +140,7 @@ class Update_Matching_Station(mixins.CreateModelMixin,viewsets.GenericViewSet):
 	def create(self,request):
 		serializer = self.get_serializer(data=request.data)
 		if serializer.is_valid():
-			mc_obj = Matching.objects.filter(pk = serializer.data['matching_id'])
+			mc_obj = Matching.objects.get(pk = serializer.data['matching_id'])
 			if mc_obj.current_station == len(mc_obj.sequence.split('->')-1): #success traveling
 				mc_obj.travel_data.status = 'done';
 				mc_obj.travel_data.account.status = 'free';
