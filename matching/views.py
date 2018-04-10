7f50e1aaa481f7dc29ac9e4ad13ce652f074ed3f
bignunxd <bignunxd@gmail.com>
2018-04-11 02:43:56 +0700

    expire time

--- matching/views.py
+++ matching/views.py
@@ -127,8 +127,7 @@ class Get_Multiple_Matching(mixins.CreateModelMixin,
 					payload = {'notification':{'body':'test','title':'test','sound':'default','priority':'high'},'data':{},'to':tmp.account.firebase_key}
 					headers = {'Content-Type':'application/json','Authorization':'key=AAAAlRsX6G8:APA91bHeUES-WUYy2bYSLzbK6td4p8xZACl_LunpyDmLEtffHD_MYkJrDii5XdfhTDX27Vr1m9YwrFL7NhJtdVHUJENur3Zf5IRD5zKduM1MH_d49zrGz77u9r6DaT2erz_Nayp_izfp'}
 					r = requests.post(url,data=payload,headers=headers)
-					temp = json.loads(r.text)
-					print(temp)
+					print(r.text)
 				travel_obj.account.status = "busy"
 				travel_obj.save()
 				#tt = Matching.objects.filter(travel_data = travel_obj,sequence = message)
