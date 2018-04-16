ccd15d9f1f97c12fdb0296ce650e27c9c78f9628
bignunxd <bignunxd@gmail.com>
2018-04-17 02:08:42 +0700

    almost done v1

--- matching/views.py
+++ matching/views.py
@@ -133,9 +133,10 @@ class Get_Multiple_Matching(mixins.CreateModelMixin,
 					tmp.save()
 					url = 'https://fcm.googleapis.com/fcm/send'
 					payload = '{"to":"'+tmp.account.firebase_key+'","data":{},"notification":{"title":"Your request has been matched","body":"idontknow","priority":"high","sound":"default"},}'
+					print(payload)
 					headers = {'Content-Type':"application/json",'Authorization':"key=AAAAlRsX6G8:APA91bHeUES-WUYy2bYSLzbK6td4p8xZACl_LunpyDmLEtffHD_MYkJrDii5XdfhTDX27Vr1m9YwrFL7NhJtdVHUJENur3Zf5IRD5zKduM1MH_d49zrGz77u9r6DaT2erz_Nayp_izfp"}
 					r = requests.post(url,data=payload,headers=headers)
-					print(r)
+					
 				travel_obj.account.status = "busy"
 				travel_obj.save()
 				#tt = Matching.objects.filter(travel_data = travel_obj,sequence = message)
