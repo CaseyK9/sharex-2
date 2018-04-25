81ef71006374cdf0f590d3e0083e7efb19dd99c4
nutchaphut <nuzchphut@gmail.com>
2018-04-25 14:26:08 +0700

    1150

--- upload/views.py
+++ upload/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from account.serializers import FileSerializer
 from account.permissions import IsCustomerAccount,IsDriverAccount,IsAuthenticated
 class FileView(APIView):
+    queryset = Signature.objects.all()
     permission_classes = (IsAuthenticated,)
     parser_classes = (MultiPartParser, FormParser)
     def post(self, request, *args, **kwargs):
