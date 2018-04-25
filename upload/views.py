4e7dc9f6cf54e208c60bb3e700c4f00801b0beeb
nutchaphut <nuzchphut@gmail.com>
2018-04-25 14:57:58 +0700

    1150

--- upload/views.py
+++ upload/views.py
@@ -9,7 +9,7 @@ from .models import Signature
 from rest_framework import viewsets, mixins
 class FileView(mixins.CreateModelMixin,
 					    viewsets.GenericViewSet):
-    #queryset = Signature.objects.all()
+    queryset = Signature.objects.all()
     permission_classes = (IsAuthenticated,)
     parser_classes = (MultiPartParser, FormParser)
     def create(self, request):
