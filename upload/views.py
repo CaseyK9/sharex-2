14abd995d713d71855f1265ff48f2c8b0249645b
nutchaphut <nuzchphut@gmail.com>
2018-04-25 14:56:17 +0700

    1150

--- upload/views.py
+++ upload/views.py
@@ -6,18 +6,16 @@ from rest_framework import status
 from account.serializers import FileSerializer
 from account.permissions import IsCustomerAccount,IsDriverAccount,IsAuthenticated
 from .models import Signature
-class FileView(APIView):
+from rest_framework import viewsets, mixins
+class FileView(mixins.CreateModelMixin,
+					    viewsets.GenericViewSet):
     #queryset = Signature.objects.all()
     permission_classes = (IsAuthenticated,)
     parser_classes = (MultiPartParser, FormParser)
-    def post(self, request, *args, **kwargs):
+    def create(self, request):
         file_serializer = FileSerializer(data=request.data)
         if file_serializer.is_valid():
             file_serializer.save()
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def get(self, request):
-        print("get fileview")
-        return Response('200')
-# Create your vigeews here.
