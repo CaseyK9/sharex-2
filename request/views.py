949300b71117fbe1a1835ebf144b4b39e32c9d2c
bignunxd <bignunxd@gmail.com>
2018-04-26 23:54:14 +0700

    asdf

--- request/views.py
+++ request/views.py
@@ -65,7 +65,7 @@ class Get_Request_Detail(mixins.CreateModelMixin,
 				detail['request_details'].append({'request_id':rq.pk,'customer_name':name,'customer_tel':rq.account.tel,'pickup_location':rq.pickup_location,'pickup_longtitude':rq.pickup_longtitude,'pickup_lattitude':rq.pickup_lattitude,'destination_location':rq.destination_location,'destination_longtitude':rq.destination_longtitude,'destination_lattitude':rq.destination_lattitude,'receiver_name':rq.receiver_name,'receiver_tel':rq.receiver_tel,'receiver_address':rq.receiver_address,'type':rq._type,'fare':rq.fare,'tracking_key':rq.tracking_key,'route_url':rq.route_url})
 				if mc_dt != None:
 					ac = mc_dt.travel.account
-					detail['driver_detials'].append({'name':ac.first_name+" "+ac.last_name,'tel':ac.tel,'license':ac.license,'rating':rating_sum/rating_number})
+					detail['driver_detials'].append({'name':ac.first_name+" "+ac.last_name,'tel':ac.tel,'license':ac.license,'rating':ac.rating_sum/ac.rating_number})
 				detail['status'].append({'status':'okay'})
 				return Response(detail)
 			else:
