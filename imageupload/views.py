8e47bbfd2a79086e38f45f7a491828f1818e88d2
bignunxd <bignunxd@gmail.com>
2018-04-27 02:28:34 +0700

    aassdd

--- imageupload/views.py
+++ imageupload/views.py
@@ -3,10 +3,12 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from account.serializers import ImageUpload
+from request.models import Request
 class Image_Upload(APIView):
   parser_classes = (MultiPartParser, FormParser)
   def post(self, request, *args, **kwargs):
-    file_serializer = ImageUpload(data=request.data)
+    file_serializer = ImageUpload(data=request.data['image'])
+    #rq = Request.objects.get()
     if file_serializer.is_valid():
       file_serializer.save()
       return Response(file_serializer.data, status=status.HTTP_201_CREATED)
