bf97bf6b1dd0d6b3c4103ba7f9f25c055f0c01d6
bignunxd <bignunxd@gmail.com>
2018-04-27 02:48:57 +0700

    aassdd

--- imageupload/views.py
+++ imageupload/views.py
@@ -12,6 +12,6 @@ class Image_Upload(APIView):
     #rq = Request.objects.get()
     if file_serializer.is_valid():
       file_serializer.save()
-      return Response(file_serializer.data+serializer.data['request_id'], status=status.HTTP_201_CREATED)
+      return Response(file_serializer.data, status=status.HTTP_201_CREATED)
     else:
       return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
