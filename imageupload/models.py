e866673485a4e619bf7c3e162a79796834a4662a
bignunxd <bignunxd@gmail.com>
2018-04-27 17:19:11 +0700

    aassdd

--- imageupload/models.py
+++ imageupload/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 class Imageupload(models.Model):
 	image = models.ImageField(upload_to="signature",blank=False, null=False)
-	rating = models.FloatField()
-	matching_id = models.IntegerField()
+	rating = models.FloatField(default=0,blank=True,null=True)
+	matching_id = models.IntegerField(default=0,blank=True,null=True)
